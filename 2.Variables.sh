1ea5ef8d352eaf458abf2cc791708b72addeeec3
vjsmit <vj.smit97@gmail.com>
2023-04-24 21:17:28 +0530

    Printing script name

--- 2.Variables.sh
+++ 2.Variables.sh
@@ -13,10 +13,4 @@ Date=$(date +%F)
 echo Today Date is ${Date}
 
 echo Hello
-echo Script name = $0
-
-script=$(realpath "$0")
-scriptpath=($dirname "$0")
-
-echo realpath is ${script}
-echo scriptpath is ${scriptpath}
+Script name $0
