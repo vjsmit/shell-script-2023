85d7372424dd04cd3135672a8018500fdd962f28
vjsmit <vj.smit97@gmail.com>
2023-05-20 16:32:52 +0530

    Printing script name

--- imdb.sh
+++ imdb.sh
@@ -3,15 +3,16 @@
 #<70% - avg movie
 
 input=$1
-
-if [ -z "$input"]; then
+if [ -z "$input" ]; then
   echo Input Movie Name Missing
   exit
 fi
 
 percent= $(curl -s https://www.themoviedb.org/movie/920125-untitled-white-men-can-t-jump-reboot | grep user_score_chart | xargs -n1 | grep data-percent | awk -F = '{print $2}' | awk -F . '{print $1}')
 
-if  ["$percent" -ge 70 ]; then
+echo percent - ${percent}
+
+if  [ "$percent" -ge 70 ]; then
   echo Good
 else
   echo Average
