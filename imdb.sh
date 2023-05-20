80aebcd2c63e56ef8321170966017155c3890f66
vjsmit <vj.smit97@gmail.com>
2023-05-20 16:33:59 +0530

    Printing script name

--- imdb.sh
+++ imdb.sh
@@ -8,7 +8,7 @@ if [ -z "$input" ]; then
   exit
 fi
 
-percent= $(curl -s https://www.themoviedb.org/movie/920125-untitled-white-men-can-t-jump-reboot | grep user_score_chart | xargs -n1 | grep data-percent | awk -F = '{print $2}' | awk -F . '{print $1}')
+percent=$(curl -s https://www.themoviedb.org/movie/920125-untitled-white-men-can-t-jump-reboot | grep user_score_chart | xargs -n1 | grep data-percent | awk -F = '{print $2}' | awk -F . '{print $1}')
 
 echo percent - ${percent}
 
