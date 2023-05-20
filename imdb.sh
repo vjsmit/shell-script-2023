1070e4ded2ad8a5adacdb4dd023e4a86735d3566
vjsmit <vj.smit97@gmail.com>
2023-05-20 16:20:19 +0530

    Printing script name

---
+++ imdb.sh
@@ -0,0 +1,19 @@
+#movie rating
+#>70% - good movie
+#<70% - avg movie
+
+input=$1
+
+if [-z "$input"]; then
+  echo Input Movie Name Missing
+  exit
+fi
+
+percent= $(curl -s https://www.themoviedb.org/movie/$input | grep user_score_chart | xargs -n1 | grep data-percent | awk -F = '{print $2}'
+         69.0)
+
+if  ["$percent" -ge 70 ]; then
+  echo Good
+else
+  echo Average
+fi
