d973ac8ba8863964f403735527a806571fe7396e
vjsmit <vj.smit97@gmail.com>
2023-05-20 16:43:22 +0530

    Printing script name

--- imdb.sh
+++ imdb.sh
@@ -10,12 +10,12 @@ fi
 
 percent=$(curl -s https://www.themoviedb.org/movie/${input}| grep user_score_chart | xargs -n1 | grep data-percent | awk -F = '{print $2}' | awk -F . '{print $1}')
 
-echo percent - ${percent}
-
 if [ -z "$percent"]; then
   echo movie name not correct
   exit
 fi
+
+echo percent - ${percent}
 
 if  [ "$percent" -ge 70 ]; then
   echo Good
